e7d498442022f9d5224740ec1d344cc750483fdd
Tessa Tapmongkol <ttapmong@calpoly.edu>
2020-12-29 12:44:29 -0800

    added bodyParser to backend

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 mongoose.connect(DATABASE_URL, {
     useNewUrlParser: true,
